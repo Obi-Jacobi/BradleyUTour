4e7e5cc6ae208f57d1ae1a72166601228242a7fe
Jacob Wilson <jacobtrwilson@icloud.com>
2017-04-06 13:54:42 -0500

    Initial Commit

---
+++ BradleyUTour/BradleyUTour/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BradleyUTour
+//
+//  Created by Jacob Wilson on 4/6/17.
+//  Copyright © 2017 Bradley University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
