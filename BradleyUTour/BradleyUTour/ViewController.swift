646ec0c21df2d4ba67acd677cef5182a92bfdf1a
Jacob Wilson <jacobtrwilson@icloud.com>
2017-04-06 18:03:05 -0500

    Added in Realm support to app

--- BradleyUTour/BradleyUTour/ViewController.swift
+++ BradleyUTour/BradleyUTour/ViewController.swift
@@ -7,11 +7,40 @@
 //
 
 import UIKit
+import RealmSwift
+
+class Dog: Object {
+    dynamic var name = ""
+    dynamic var age = 0
+}
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let myDog = Dog()
+        myDog.name = "Rex"
+        myDog.age = 1
+        print("name of dog: \(myDog.name)")
+        
+        // Get the default Realm
+        let realm = try! Realm()
+        
+        // Query Realm for all dogs less than 2 years old
+        let puppies = realm.objects(Dog.self).filter("age < 2")
+        puppies.count // => 0 because no dogs have been added to the Realm yet
+        
+        // Persist your data easily
+        try! realm.write {
+            realm.add(myDog)
+        }
+        
+        let thing = realm.objects(Dog.self)
+        print(thing)
+        
+        print(Realm.Configuration.defaultConfiguration.fileURL!)
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
