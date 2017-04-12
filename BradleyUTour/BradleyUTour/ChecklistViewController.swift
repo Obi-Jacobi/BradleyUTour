6bc1196d96e5cc345409368fd96c60e1493ed90a
Jacob Wilson <jacobtrwilson@icloud.com>
2017-04-12 13:15:51 -0500

    Added in a ChecklistViewController which allows for the progressLabel to update its position based on how big the progressBar is

---
+++ BradleyUTour/BradleyUTour/ChecklistViewController.swift
@@ -0,0 +1,72 @@
+//
+//  ChecklistViewController.swift
+//  BradleyUTour
+//
+//  Created by Jacob Wilson on 4/12/17.
+//  Copyright © 2017 Bradley University. All rights reserved.
+//
+
+import UIKit
+
+class ChecklistViewController: UIViewController {
+    
+    @IBOutlet var progressBar: UIProgressView!
+    @IBOutlet var progressLabel: UILabel!
+    
+    // This is how you call it to reload the progress label location 
+    /*
+    @IBAction func test(_ sender: Any) {
+        let thing = sender as! UIStepper
+        let value = thing.value
+        
+        progressBar.progress = Float(value)/10
+        viewDidLayoutSubviews()
+    }*/
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidLayoutSubviews() {
+        let barRect = progressBar.frame
+        let labelRect = progressLabel.frame
+        let barValue = progressBar.progress
+        
+        let progressWidth = Float(barRect.width) * barValue
+        var newLabelRect = labelRect
+        newLabelRect.origin.x = CGFloat(progressWidth/2) - (labelRect.width / 2)
+        //newLabelRect.midX
+       
+        
+        progressLabel.frame = newLabelRect
+        
+        /*
+        print(barRect)
+        print(progressWidth)
+        print(labelRect)
+        print(labelRect.width)
+        print(newLabelRect)
+        print(newLabelRect.midX)*/
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
