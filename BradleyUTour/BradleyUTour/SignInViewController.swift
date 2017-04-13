9ea9727a67d7b537a9ae0cefd257ea8d38a51695
Jacob Wilson <jacobtrwilson@icloud.com>
2017-04-12 23:56:57 -0500

    - Updated SigninViewController to allow for dismissal of keyboard both when tapping outside it and also when hitting the done button. - SigninViewController’s UI has also been updated - Added an extension for the UITextField dismissal stuff. We may end up removing this but for now it might aid in the headache of setting it up. What may be the better decision in the future would be to add a intermediate parent class with members scrollView and activeField so that it solves the problem of only writing the code once and being available to all other subclasses since and Extension to a class cannot contain stored properties and this breaks when trying to move all of the code into one extension since it needs a scrollView and activeField

--- BradleyUTour/BradleyUTour/SignInViewController.swift
+++ BradleyUTour/BradleyUTour/SignInViewController.swift
@@ -7,13 +7,169 @@
 //
 
 import UIKit
+import RealmSwift
+
+class User: Object {
+    dynamic var firstName = ""
+    dynamic var lastName = ""
+    dynamic var email = ""
+}
+
+protocol KeyboardDismissable {
+    func keyboardWasShown(notification: NSNotification)
+    func keyboardWillBeHidden(notification: NSNotification)
+}
+
+extension UIViewController: UITextFieldDelegate, KeyboardDismissable {    
+    func keyboardWasShown(notification: NSNotification) {}
+    func keyboardWillBeHidden(notification: NSNotification) {}
+
+    func hideKeyboardWhenTapped() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+    func registerKeyboard() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWasShown(notification:)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillBeHidden(notification:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    }
+    
+    func deregisterKeyboard(){
+        //Removing notifies on keyboard appearing
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    }
+    /*
+    func keyboardWasShown(notification: NSNotification){
+        //Need to calculate keyboard exact size due to Apple suggestions
+        scrollView.isScrollEnabled = true
+        var info = notification.userInfo!
+        let keyboardSize = (info[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue.size
+        let contentInsets : UIEdgeInsets = UIEdgeInsetsMake(0.0, 0.0, keyboardSize!.height, 0.0)
+        
+        scrollView.contentInset = contentInsets
+        scrollView.scrollIndicatorInsets = contentInsets
+        
+        var aRect : CGRect = self.view.frame
+        aRect.size.height -= keyboardSize!.height
+        if let activeField = activeField {
+            if (!aRect.contains(activeField.frame.origin)){
+                self.scrollView.scrollRectToVisible(activeField.frame, animated: true)
+            }
+        }
+    }
+    
+    func keyboardWillBeHidden(notification: NSNotification){
+        //Once keyboard disappears, restore original positions
+        var info = notification.userInfo!
+        let keyboardSize = (info[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue.size
+        let contentInsets : UIEdgeInsets = UIEdgeInsetsMake(0.0, 0.0, -keyboardSize!.height, 0.0)
+        scrollView.contentInset = contentInsets
+        scrollView.scrollIndicatorInsets = contentInsets
+        view.endEditing(true)
+        scrollView.isScrollEnabled = false
+    }*/
+}
 
 class SignInViewController: UIViewController {
-
+    
+    @IBOutlet var startButton: UIButton!
+    @IBOutlet var firstName: UITextField!
+    @IBOutlet var lastName: UITextField!
+    @IBOutlet var email: UITextField!
+    
+    @IBOutlet var scrollView:UIScrollView!
+    var activeField:UITextField?
+    
+    //MARK: UITextFieldDismissing stuff
+    override func keyboardWasShown(notification: NSNotification){
+        //Need to calculate keyboard exact size due to Apple suggestions
+        scrollView.isScrollEnabled = true
+        var info = notification.userInfo!
+        let keyboardSize = (info[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue.size
+        let contentInsets : UIEdgeInsets = UIEdgeInsetsMake(0.0, 0.0, keyboardSize!.height, 0.0)
+        
+        scrollView.contentInset = contentInsets
+        scrollView.scrollIndicatorInsets = contentInsets
+        
+        var aRect : CGRect = self.view.frame
+        aRect.size.height -= keyboardSize!.height
+        if let activeField = activeField {
+            if (!aRect.contains(activeField.frame.origin)){
+                self.scrollView.scrollRectToVisible(activeField.frame, animated: true)
+            }
+        }
+    }
+    
+    override func keyboardWillBeHidden(notification: NSNotification){
+        //Once keyboard disappears, restore original positions
+        var info = notification.userInfo!
+        let keyboardSize = (info[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue.size
+        let contentInsets : UIEdgeInsets = UIEdgeInsetsMake(0.0, 0.0, -keyboardSize!.height, 0.0)
+        scrollView.contentInset = contentInsets
+        scrollView.scrollIndicatorInsets = contentInsets
+        view.endEditing(true)
+        scrollView.isScrollEnabled = false
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField){
+        activeField = textField
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField){
+        activeField = nil
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    //MARK: SigninViewController Methods
+    
+    @IBAction func startButtonPressed(_ sender: UIButton) {
+        let realm = try! Realm()
+        
+        let user = User()
+        
+        guard let firstNameText = firstName.text
+            else {
+                return
+        }
+        user.firstName = firstNameText
+        guard let emailText = email.text
+            else {
+                return
+        }
+        user.email = emailText
+        
+        try! realm.write {
+            realm.add(user)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        registerKeyboard()
+        hideKeyboardWhenTapped()
+        
+        print(Realm.Configuration.defaultConfiguration.fileURL!)
+        
+        startButton.layer.cornerRadius = 15
+        startButton.layer.borderWidth = 1
+        startButton.layer.borderColor = UIColor.black.cgColor
+        
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        deregisterKeyboard()
     }
 
     override func didReceiveMemoryWarning() {
