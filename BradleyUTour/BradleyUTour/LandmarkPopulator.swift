44da2bf3a21e1e41ac26a4424fe3f4d5b5c37c77
Jacob Wilson <jacobtrwilson@icloud.com>
2017-04-13 15:45:55 -0500

    ### Added remaining Landmark locations from the Google Drive
    Specific Changes Made:
    *

--- BradleyUTour/BradleyUTour/LandmarkPopulator.swift
+++ BradleyUTour/BradleyUTour/LandmarkPopulator.swift
@@ -13,13 +13,30 @@ struct LandmarkPopulator {
     
     private let founders = Landmark(value:
         ["Founder's Circle", false, "This is Founder's Circle", 40.698143, -89.616412, NSData()])
-        
+    private let bradleyHall = Landmark(value:
+        ["Bradley Hall", false, "This is Bradley Hall", 40.698312, -89.617012, NSData()])
+    private let gcc = Landmark(value:
+        ["Caterpillar Global Communications Center", false, "This is the GCC", 40.697943, -89.612109, NSData()])
+    private let jobst = Landmark(value:
+        ["Jobst Hall", false, "This is Jobst Hall", 40.699366, -89.616985, NSData()])
+    private let westlake = Landmark(value:
+        ["Westlake Hall", false, "This is Westlake Hall", 40.697646, -89.617006, NSData()])
+    private let baker = Landmark(value:
+        ["Baker Hall", false, "This is Westlake Hall", 40.699760, -89.616004, NSData()])
+    private let studentCenter = Landmark(value:
+        ["Michel Student Center", false, "This is the Student Center", 40.698720, -89.615148, NSData()])
+    private let library = Landmark(value:
+        ["Cullom-Davis Library", false, "This is the Library", 40.697093, -89.616636, NSData()])
     
     func populateRealm() {
-        let database = [founders]
+        let database = [founders, bradleyHall, gcc, jobst, westlake, baker, studentCenter, library]
         
-        for landmark in database {
-            print(landmark)
+        let realm = try! Realm()
+        
+        try! realm.write {
+            for landmark in database {
+                realm.add(landmark)
+            }
         }
     }
 }
